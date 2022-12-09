76a0900c2f735ebd38ec2a13b573e7c30fe83e96
vasylcode <vasylcode@gmail.com>
2022-12-09 21:43:31 +0200

    17.2 upd

--- setup.sh
+++ setup.sh
@@ -8,13 +8,13 @@ source $HOME/.profile
 sudo systemctl stop massa
 rm -rf $HOME/massa
 
-wget https://github.com/massalabs/massa/releases/download/TEST.17.0/massa_TEST.17.0_release_linux.tar.gz
-tar zxvf massa_TEST.17.0_release_linux.tar.gz -C $HOME/
+wget https://github.com/massalabs/massa/releases/download/TEST.17.2/massa_TEST.17.2_release_linux.tar.gz
+tar zxvf massa_TEST.17.2_release_linux.tar.gz -C $HOME/
 
 sudo tee <<EOF >/dev/null /etc/systemd/system/massa.service
 [Unit]
 Description=Massa Node
-After=network-online.target
+After=network-online.target  
 
 [Service]
 User=$USER
@@ -36,12 +36,12 @@ echo "alias client='cd $HOME/massa/massa-client/ && $HOME/massa/massa-client/mas
 echo "alias clientw='cd $HOME/massa/massa-client/ && $HOME/massa/massa-client/massa-client --pwd $MASSA_PASS && cd'" >> ~/.profile
 
 if [ "$language" = "uk" ]; then
-    echo -e "\n\e[93mMassa TEST 17 \e[0m\n"
+    echo -e "\n\e[93mMassa TEST 17.2 \e[0m\n"
     echo -e "Подивитись логи ноди \e[92mjournalctl -u massa -f -o cat\e[0m"
     echo -e "\e[92mCTRL + C\e[0m щоб вийти з логів\n"
     echo -e "Зайти в меню ноди \e[92msource .profile && clientw\e[0m"
 else
-    echo -e "\n\e[93mMassa TEST 17 \e[0m\n"
+    echo -e "\n\e[93mMassa TEST 17.2 \e[0m\n"
     echo -e "Check node logs \e[92mjournalctl -u massa -f -o cat\e[0m"
     echo -e "\e[92mCTRL + C\e[0m to exit logs\n"
     echo -e "Open node menu \e[92msource .profile && clientw\e[0m"
